4c465835c60104168bd0d4954bc0af2c68e8c4f3
Ethan <vya9tu@virginia.edu>
2023-05-18 00:50:53 -0400

    hero

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ app.get('/user',(req,res) => {
   const token = 'https://api.spotify.com/v1/me'
   const authHeader = req.headers.authorization;
   console.log(authHeader)
+  if(!atop){
+    res.json(authHeader)
+  }
   const atoken = authHeader && authHeader.split(" ")[1];
   console.log(atoken)
     const jwtPayload = jwt.verify(atoken, jwtSecret);
